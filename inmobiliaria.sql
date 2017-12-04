34317f15f8bdda80a06f2975a0b2c07ce58ee2b6
TheJosecraft <josekawa1996@gmail.com>
2017-12-05 00:45:29 +0100

    Add files via upload

--- inmobiliaria.sql
+++ inmobiliaria.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 22-11-2017 a las 14:55:44
--- Versión del servidor: 10.1.13-MariaDB
--- Versión de PHP: 5.6.23
+-- Tiempo de generación: 05-12-2017 a las 00:44:42
+-- Versión del servidor: 10.1.26-MariaDB
+-- Versión de PHP: 7.1.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -35,13 +37,6 @@ CREATE TABLE `citas` (
   `id_cliente` bigint(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Volcado de datos para la tabla `citas`
---
-
-INSERT INTO `citas` (`id`, `fecha`, `hora`, `motivo`, `lugar`, `id_cliente`) VALUES
-(1, '2017-11-14', '13:20:00', 'Concretar el precio del inmueble', 'Calle Primavera', 1);
-
 -- --------------------------------------------------------
 
 --
@@ -62,9 +57,7 @@ CREATE TABLE `clientes` (
 --
 
 INSERT INTO `clientes` (`id`, `nombre`, `apellidos`, `direccion`, `telefono1`, `telefono2`) VALUES
-(0, 'Disponible', '', '', '', ''),
-(1, 'José Carlos', 'Raya León', 'Avenida de la Constitución 17, 3P', '638564461', '957655755'),
-(4, 'Álvaro', 'Raya León', 'Avenida de la Constitución 17, 3P', '638862638', '957655755');
+(0, 'Disponible', '', '', '', '');
 
 -- --------------------------------------------------------
 
@@ -75,7 +68,7 @@ INSERT INTO `clientes` (`id`, `nombre`, `apellidos`, `direccion`, `telefono1`, `
 CREATE TABLE `inmuebles` (
   `id` bigint(20) UNSIGNED NOT NULL,
   `direccion` varchar(100) NOT NULL,
-  `descripcion` varchar(280) NOT NULL,
+  `descripcion` varchar(560) NOT NULL,
   `precio` decimal(12,2) NOT NULL,
   `id_cliente` bigint(20) NOT NULL,
   `imagen` varchar(50) NOT NULL
@@ -86,8 +79,10 @@ CREATE TABLE `inmuebles` (
 --
 
 INSERT INTO `inmuebles` (`id`, `direccion`, `descripcion`, `precio`, `id_cliente`, `imagen`) VALUES
-(6, 'Calle la Parra 5, 3C', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in volupt', '50000.00', 0, 'inmueble6.jpg'),
-(7, 'Calle la Parra 5, 3C', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in volupt', '75000.00', 1, 'inmueble7.jpg');
+(1, 'San José de la Atalaya', 'Esta villa completamente reformada está ubicada al sur de la isla, cerca de San José; bien ubicada para encontrar paz y privacidad. La propiedad ha sido diseñada y renovada para que rezume un estilo ibicenco moderno y está distribuida en dos pisos.', '3500000.00', 0, 'inmueble1.jpg'),
+(2, 'Avda de Asturias 81, Torrelodones', 'Chalet independiente de diseño en tres alturas construido en el año 2007 con vistas panorámicas del noroeste de Madrid y la sierra de Gredos. Situado en La Berzosa, urbanización privada con servicio de vigilancia 24 h.', '975000.00', 0, 'inmueble2.jpg'),
+(3, 'Villa en Tarragona', 'Vivienda de diseño con vistas al mar en Cala Romana, Tarragona.Esta magnífica propiedad de lujo está situada en la urbanización más cercana a Tarragona, Cala Romana, zona de prestigio y muy próxima a las playas.', '1300000.00', 0, 'inmueble3.jpg'),
+(4, 'Mansión en la Zagaleta, Málaga', 'Casa-palacio estilo árabe en la Zagaleta. La propiedad ofertada, con una superficie construida de 2.160 m2 en una parcela de 6.700 m2, cuenta con las mejores calidades, desde muralla de piedra exterior escalonada hasta artesonados de madera policromada en los techos.', '15500000.00', 0, 'inmueble4.jpg');
 
 -- --------------------------------------------------------
 
@@ -97,7 +92,7 @@ INSERT INTO `inmuebles` (`id`, `direccion`, `descripcion`, `precio`, `id_cliente
 
 CREATE TABLE `noticias` (
   `id` bigint(20) UNSIGNED NOT NULL,
-  `titular` varchar(50) NOT NULL,
+  `titular` varchar(100) NOT NULL,
   `contenido` varchar(1500) NOT NULL,
   `imagen` varchar(50) NOT NULL,
   `fecha` date NOT NULL
@@ -108,10 +103,7 @@ CREATE TABLE `noticias` (
 --
 
 INSERT INTO `noticias` (`id`, `titular`, `contenido`, `imagen`, `fecha`) VALUES
-(1, 'Se vende piso', 'Buenas a todos! He encontrado un piso muy bien de precio y he decidido compartirlo con vosotros.\r\n\r\nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\r\n\r\nSed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?', 'noticia1.jpg', '2017-11-13'),
-(2, 'Se busca psio', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\r\n\r\nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 'noticia2.jpg', '2017-11-15'),
-(3, 'Buenas a todos', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\r\n\r\nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 'noticia3.jpg', '2017-11-16'),
-(4, 'Nuevo inmueble', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\r\n\r\nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 'noticia4.jpg', '2017-11-16');
+(1, 'La actividad inmobiliaria en España está un 30% por debajo de los máximos de 2007', 'El sector inmobiliario y de la construcción en España registró en el tercer trimestre de 2017 unos niveles de actividad un 30% por debajo de los máximos registrados en 2007, según el nuevo Indice Registral de Actividad Inmobiliaria (IRAI), un indicador elaborado por el Colegio de Registradores para medir globalmente la actividad del sector.\r\n\r\nEn el último trimestre, los datos muestran un IRAI de 98,26 frente al 139,90 registrado el año previo al estallido de la última gran crisis económica. Sin embargo, desde los mínimos históricos de 68 a los que cayó en 2013, el sector ya ha recuperado un 45% hasta ahora.\r\n\r\nSolo en lo que va de año, la actividad inmobiliaria y de la construcción ha avanzado un 10%, partiendo de índice IRAI de 89 que se registró en diciembre del pasado año. Los grupos con una mayor repercusión positiva en este 10% de incremento durante los nueve primeros meses del año han sido la compraventa, especialmente de viviendas nuevas y usadas y el número de hipotecas registrado. En concreto, la compraventa de viviendas nuevas ha crecido en estos nueve meses un 32%, mientras que la usada lo ha hecho en un 27%. En cuanto al número de hipotecas, el avance ha sido del 16%. Por su parte, desde el punto de vista mercantil, el mayor impulso de la actividad se debe al descenso en el número de concurso de acreedores, tanto de empresas de construcción, que ha bajado un 83%, como en sociedades inmobiliarias, que ha descendido un 57%.', 'noticia1.jpg', '2017-12-04');
 
 --
 -- Índices para tablas volcadas
@@ -153,22 +145,27 @@ ALTER TABLE `noticias`
 -- AUTO_INCREMENT de la tabla `citas`
 --
 ALTER TABLE `citas`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT de la tabla `clientes`
 --
 ALTER TABLE `clientes`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT de la tabla `inmuebles`
 --
 ALTER TABLE `inmuebles`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
 --
 -- AUTO_INCREMENT de la tabla `noticias`
 --
 ALTER TABLE `noticias`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
